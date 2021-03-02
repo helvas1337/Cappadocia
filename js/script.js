ce6f75865926afda8e95b536617369308e335387
helvas1337 <helvas1998@gmail.com>
2021-03-03 01:52:36 +0200

    add two section with two slider

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function() {
+    $(".fairy-tail__slider").slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/arrow-prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/arrow-next.svg"></button>',
+        autoplay: true,
+        fade: true,
+    });
+    $(".our-trip__slider").slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/arrow-prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/arrow-next.svg"></button>',
+        autoplay: true,
+        fade: true,
+    });
+});
+
